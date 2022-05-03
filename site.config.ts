c0419c732603411c89c127c866cfab6ffd5be4bc
Peng He <34941901+kamirdin@users.noreply.github.com>
2022-05-03 22:23:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '461d7ace0fb742a8a4827655c3842052',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'kamirdin',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
