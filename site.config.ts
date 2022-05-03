a02955634772eab840da6af2e8a3a910395fa037
Peng He <34941901+kamirdin@users.noreply.github.com>
2022-05-03 23:37:25 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '461d7ace0fb742a8a4827655c3842052',
+  rootNotionPageId: '5262cee6bd3b4e3ba6e410d2efcabba2',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'My Blogs',
+  domain: 'hepeng.notes',
+  author: 'Peng He',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'My Notes About My Things',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
+  twitter: 'kamirdin',
   github: 'kamirdin',
-  linkedin: 'fisch2',
+  linkedin: null,
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
